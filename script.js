0b321c59e59ff6f98ee8fd81dbe2d519b991072d
honoredisaac <superelectrohdzzz@gmail.com>
2025-04-16 17:29:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const canv = document.getElementById('canv').getContext("2d");
 document.getElementById('canv').width = window.innerWidth;
 document.getElementById('canv').height = window.innerHeight;
 const billypos = {x: 0,y:0};
-const oldpos = {x: 0,y: 0}
-let speed = 5;
-
+const oldpos = {x: 0,y: 0};
+let foodpos = {x:0,y:0};
+let speed = 1;
 function clearcanvas(){
   canv.clearRect(0, 0, canv.canvas.width, canv.canvas.height);
+  projectfood(Math.random()*(canv.canvas.width-475)+475,Math.random()*(canv.canvas.height-475)+475);
 }
 
 function projectbilly(x,y){
@@ -32,20 +33,31 @@ function projectbilly(x,y){
   canv.stroke();
   canv.closePath();
 }
+function projectfood(x,y){
+canv.fillStyle = "rgba(0,0,0,0.5)";
+canv.fillRect(x,y,25,25)
+canv.fillStyle = "black";
+foodpos.x = x;foodpos.y = y;
+}
 projectbilly(0,0);
+projectfood(Math.random()*(canv.canvas.width-475)+475,Math.random()*(canv.canvas.height-475)+475);
 document.addEventListener("keydown",(key)=>{
   switch (key.code) {
     case 'KeyW':
       projectbilly(billypos.x,billypos.y - speed)
+      speed = speed+(22-speed)*(0.000001)
       break;
     case 'KeyA':
       projectbilly(billypos.x - speed,billypos.y)
+      speed = speed+(22-speed)*(0.000001)
       break;
     case 'KeyS':
       projectbilly(billypos.x,billypos.y + speed)
+      speed = speed+(22-speed)*(0.000001)
       break;
     case 'KeyD':
       projectbilly(billypos.x + speed,billypos.y)
+      speed = speed+(22-speed)*(0.000001)
       break;
     case 'KeyE':
         clearcanvas();
