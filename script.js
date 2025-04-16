29ad3f690cc14b632614c3d285af2c48aaca66e9
honoredisaac <superelectrohdzzz@gmail.com>
2025-04-16 18:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ function clearcanvas(){
   canv.clearRect(0, 0, canv.canvas.width, canv.canvas.height);
   projectfood(Math.random()*(canv.canvas.width-475)+475,Math.random()*(canv.canvas.height-475)+475);
 }
-
+function iro(r1, r2) {
+  return !(
+    r1.x + r1.width < r2.x ||
+    r1.x > r2.x + r2.width ||
+    r1.y + r1.height < r2.y ||
+    r1.y > r2.y + r2.height
+  );
+}
+function projectfood(x,y){
+canv.fillStyle = "rgba(0,0,0,0.5)";
+canv.fillRect(x,y,25,25)
+canv.fillStyle = "black";
+foodpos.x = x;foodpos.y = y;
+}
 function projectbilly(x,y){
   oldpos.x = billypos.x;
   oldpos.y = billypos.y;
@@ -32,15 +45,19 @@ function projectbilly(x,y){
   canv.lineTo(x+66.5,y+50);
   canv.stroke();
   canv.closePath();
+  const billyBox = { x: billypos.x, y: billypos.y, width: 100, height: 100 };
+  const foodBox = { x: foodpos.x, y: foodpos.y, width: 25, height: 25 };
+  if (iro(billyBox, foodBox)) {
+    projectfood(Math.random()*canv.canvas.width,Math.random()*canv.canvas.height);
+    canv.clearRect(foodpos.x,foodpos.y,25,25);
+    projectbilly(billypos.x,billypos.y);
+    speed += 1;
+    document.getElementById('h1').innerText = Number(document.getElementById('h1').innerText)+1
+  }
 }
-function projectfood(x,y){
-canv.fillStyle = "rgba(0,0,0,0.5)";
-canv.fillRect(x,y,25,25)
-canv.fillStyle = "black";
-foodpos.x = x;foodpos.y = y;
-}
+
 projectbilly(0,0);
-projectfood(Math.random()*(canv.canvas.width-475)+475,Math.random()*(canv.canvas.height-475)+475);
+projectfood(Math.random()*canv.canvas.width,Math.random()*canv.canvas.height);
 document.addEventListener("keydown",(key)=>{
   switch (key.code) {
     case 'KeyW':
